17a2522692fd7aa2340ff81cdb0f0edea22be304
marco981 <marco98x2@gmail.com>
2019-08-19 21:41:11 +0100

    basic style for sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
+import './Sidebar.scss'
+
 export default class Sidebar extends Component {
   constructor (props) {
     super(props)
   }
 
   render () {
-    return <div>sidebar</div>
+    const className = this.props.isOn ? 'Sidebar active' : 'Sidebar'
+    return (
+      <div className={className}>
+        <h3>WHATSAROUNDME</h3>
+        <input type='text' />
+        <div className='results' />
+      </div>
+    )
   }
 }
