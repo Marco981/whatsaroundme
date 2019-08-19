c8a7a4f0fac283d4505f3cd53866379c174a3621
marco981 <marco98x2@gmail.com>
2019-08-19 21:40:52 +0100

    basic style for Mapcontainer

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 
+import './MapContainer.scss'
+
 class MapContainer extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      results: [],
+      sidebarOn: false
+    }
+    this.toggleSidebar = this.toggleSidebar.bind(this)
+  }
+
+  toggleSidebar () {
+    this.setState({
+      sidebarOn: !this.state.sidebarOn
+    })
   }
 
   render () {
     return (
-      <div>
-        <Sidebar />
+      <div className='MapContainer'>
+        <div className='menu' onClick={this.toggleSidebar}>
+          <span className='menu__item' />
+          <span className='menu__item' />
+          <span className='menu__item' />
+        </div>
+        <Sidebar isOn={this.state.sidebarOn} />
       </div>
     )
   }
