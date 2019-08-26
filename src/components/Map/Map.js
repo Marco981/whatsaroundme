0384aa4fff406437183b3a19029b0a6d6205c60b
marco981 <marco98x2@gmail.com>
2019-08-26 19:32:13 +0100

    remove geolocation and add markers and popups

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,33 +1,61 @@
 import React, { useState } from 'react'
-import MapGL, { NavigationControl, GeolocateControl } from 'react-map-gl'
-const Map = _ => {
-  const geolocateStyle = {
-    position: 'absolute',
-    right: '8px',
-    top: '8px'
-  }
+import MapGL, { Marker, Popup } from 'react-map-gl'
+
+import './Map.scss'
+
+const Map = props => {
   const _onViewportChange = viewport =>
     setViewPort({ ...viewport, transitionDuration: 1000 })
   const [viewport, setViewPort] = useState({
     width: '100%',
-    height: 900,
-    latitude: 0,
-    longitude: 0,
-    zoom: 2
+    height: '100vh',
+    latitude: 51.5036722984105,
+    longitude: -0.09756599223727154,
+    zoom: 12
   })
+
+  const places = props.places.map(poi => {
+    return (
+      <Marker
+        key={poi.id}
+        latitude={poi.geometry.location.lat}
+        longitude={poi.geometry.location.lng}
+      >
+        <button
+          className='PoiButton'
+          onClick={() => props.setSelectedResult(poi)}
+        >
+          <img src={poi.icon} alt={poi.name} />
+        </button>
+      </Marker>
+    )
+  })
+
+  const selectedResult = props.selectedResult
 
   return (
     <MapGL
       {...viewport}
-      mapStyle='mapbox://styles/mapbox/dark-v9'
+      mapStyle='mapbox://styles/mapbox/dark-v10'
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={_onViewportChange}
     >
-      <GeolocateControl
-        style={geolocateStyle}
-        positionOptions={{ enableHighAccuracy: true }}
-        trackUserLocation
-      />
+      {places}
+
+      {selectedResult && (
+        <Popup
+          className='Popup'
+          onClose={() => {
+            props.setSelectedResult('')
+          }}
+          latitude={selectedResult.geometry.location.lat}
+          longitude={selectedResult.geometry.location.lng}
+        >
+          <h4>{selectedResult.name}</h4>
+          <p>Users Rating: {selectedResult.rating} / 5</p>
+          <p>{selectedResult.formatted_address}</p>
+        </Popup>
+      )}
     </MapGL>
   )
 }
