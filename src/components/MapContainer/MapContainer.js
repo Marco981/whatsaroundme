8c6903e58dcc6b1a22a6a4a0ce940d740b0efe9b
marco981 <marco98x2@gmail.com>
2019-08-26 19:33:49 +0100

    handle changes in input and selected POI

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import Map from '../Map/Map'
+import * as poiData from '../../data/pointsOfInterest.json'
 
 import './MapContainer.scss'
 
@@ -9,14 +10,39 @@ class MapContainer extends Component {
     super(props)
     this.state = {
       results: [],
-      sidebarOn: false
+      sidebarOn: false,
+      selectedResult: '',
+      inputValue: ''
     }
     this.toggleSidebar = this.toggleSidebar.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.setSelectedResult = this.setSelectedResult.bind(this)
   }
 
   toggleSidebar () {
     this.setState({
       sidebarOn: !this.state.sidebarOn
+    })
+  }
+
+  handleInputChange (e) {
+    this.setState({ selectedResult: '' })
+    const inputValue = e.target.value
+    const results = poiData.features
+    const regex = new RegExp(inputValue, 'gi')
+    const matches = results.filter(result => {
+      return result.name.match(regex)
+    })
+    this.setState({ inputValue: e.target.value, results: matches })
+  }
+
+  setSelectedResult (poi) {
+    this.setState({ selectedResult: poi, sidebarOn: false })
+  }
+
+  componentDidMount () {
+    this.setState({
+      results: poiData.features
     })
   }
 
@@ -28,8 +54,18 @@ class MapContainer extends Component {
           <span className='menu__item' />
           <span className='menu__item' />
         </div>
-        <Sidebar isOn={this.state.sidebarOn} />
-        <Map />
+        <Sidebar
+          inputValue={this.state.inputValue}
+          changeValue={this.handleInputChange}
+          places={this.state.results}
+          isOn={this.state.sidebarOn}
+          setSelectedResult={this.setSelectedResult}
+        />
+        <Map
+          selectedResult={this.state.selectedResult}
+          places={this.state.results}
+          setSelectedResult={this.setSelectedResult}
+        />
       </div>
     )
   }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,17 +3,23 @@ import React, { Component } from 'react'
 import './Sidebar.scss'
 
 export default class Sidebar extends Component {
-  constructor (props) {
-    super(props)
-  }
-
   render () {
+    const places = this.props.places.slice(0, 10).map(poi => (
+      <li onClick={() => this.props.setSelectedResult(poi)} key={poi.id}>
+        {poi.name}
+      </li>
+    ))
     const className = this.props.isOn ? 'Sidebar active' : 'Sidebar'
     return (
       <div className={className}>
         <h3>WHATSAROUNDME</h3>
-        <input type='text' />
-        <div className='results' />
+        <input
+          placeholder='search in London'
+          type='text'
+          value={this.props.selectedResult}
+          onChange={this.props.changeValue}
+        />
+        <ul className='results'>{places}</ul>
       </div>
     )
   }
